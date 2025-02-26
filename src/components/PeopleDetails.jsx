fae675aa895365be235848819e740da336856d75
Arnob Barua <baruaarnob45@gmail.com>
2025-02-26 09:24:06 +0800

    complete Adding all Part

--- src/components/PeopleDetails.jsx
+++ src/components/PeopleDetails.jsx
@@ -1,29 +1,159 @@
-// filepath: /C:/Users/ASUS/Desktop/SCSDB-Project/src/components/PeopleDetails.jsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import { asyncloadperson, removeperson } from "../store/actions/personActions";
 import Loading from "./Loading";
 import HorizontalCards from "./partials/HorizontalCards";
+import Dropdown from "./partials/Dropdown";
 
 const PeopleDetails = () => {
-  const { pathname } = useLocation();
   const navigate = useNavigate();
   const { id } = useParams();
   const { info } = useSelector((state) => state.people); // Ensure this matches the state structure
   const dispatch = useDispatch();
-
-  console.log(info);
+  const [category, setcategory] = useState("movie");
 
   useEffect(() => {
     dispatch(asyncloadperson(id));
     return () => {
       dispatch(removeperson());
     };
-  }, [id, dispatch]);
+  }, [id]);
 
   return info ? (
-    <div className="text-white">PeopleDetails</div>
+    <div className="px-[8%] h-[170vh] w-screen bg-[#1F1E24]">
+      {" "}
+      <nav className="h-[10vh]  w-full text-zinc-100 flex items-center gap-10 text-xl">
+        <Link
+          onClick={() => navigate(-1)}
+          className="hover:text-[#6556CD] ri-arrow-left-line"
+        ></Link>
+      </nav>
+      <div className="w-full flex">
+        {/* part-2 */}
+        <div className="w-[18%]">
+          <img
+            className="shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover rounded-md"
+            src={`https://image.tmdb.org/t/p/original/${info.detail.profile_path}`}
+            alt=""
+          />
+          <hr className="mt-10 mb-5 border-none h-[2px] bg-zinc-500" />
+
+          {/* Social Link and ALl */}
+          <div className="text-2xl text-white flex gap-x-5">
+            <a
+              target="_blank"
+              href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}
+            >
+              <i className="hover:text-[#6556CD]  ri-earth-fill"></i>
+            </a>
+
+            <a
+              target="_blank"
+              href={`https://www.facebook.com/${info.externalid.facebook_id}`}
+            >
+              <i className="hover:text-[#6556CD]  ri-facebook-circle-fill"></i>
+            </a>
+
+            <a
+              target="_blank"
+              href={`https://www.instagram.com/${info.externalid.instagram_id}`}
+            >
+              <i className="hover:text-[#6556CD]  ri-instagram-fill"></i>
+            </a>
+
+            <a
+              target="_blank"
+              href={`https://www.twitter.com/${info.externalid.twitter_id}`}
+            >
+              <i className="hover:text-[#6556CD]  ri-twitter-x-fill"></i>
+            </a>
+          </div>
+
+          {/* Personal Information */}
+          <h1 className="text-2xl text-zinc-400 font-semibold my-5">
+            {" "}
+            Person Info{" "}
+          </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold ">Know For</h1>
+          <h1 className="text-zinc-400">{info.detail.known_for_department} </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-3">Gender</h1>
+          <h1 className="text-zinc-400">
+            {info.detail.gender === 2 ? "Male" : "Female"}{" "}
+          </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-3">Birthday</h1>
+          <h1 className="text-zinc-400">{info.detail.birthday} </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-3">Deathday</h1>
+          <h1 className="text-zinc-400">
+            {info.detail.deathday ? info.detail.deathday : "Still Alive"}{" "}
+          </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-3">
+            Place of Birth
+          </h1>
+          <h1 className="text-zinc-400">{info.detail.place_of_birth} </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-3">
+            Also Known As
+          </h1>
+          <h1 className="text-zinc-400">
+            {info.detail.also_known_as.join(", ")}{" "}
+          </h1>
+        </div>
+
+        {/* part-3 */}
+        <div className=" w-[82%] ml-[5%] ">
+          <h1 className="text-6xl text-zinc-400 font-black my-5">
+            {" "}
+            {info.detail.name}{" "}
+          </h1>
+
+          <h1 className="text-xl text-zinc-400 font-semibold ">Biography</h1>
+          <h1 className="text-zinc-400 mt-3">{info.detail.biography} </h1>
+
+          <h1 className="text-lg text-zinc-400 font-semibold mt-5 ">
+            Know For
+          </h1>
+          <HorizontalCards data={info.combinedCredits.cast} />
+
+          <div className="w-full flex justify-between">
+            <h1 className="mt-5 text-xl text-zinc-600 font-semibold">Acting</h1>
+
+            <Dropdown
+              title="Category"
+              options={["tv", "movie"]}
+              func={(e) => setcategory(e.target.value)}
+              value={category}
+            />
+          </div>
+
+          <div className="list-disc text-zinc-400 w-full h-[50vh] mt-5 overflow-x-hidden overflow-y-auto shadow-xl shadow-[rgba(255,255,255,.3)] border-2 border-zinc-700 p-5">
+            {info[category + "Credits"].cast.map((c, i) => (
+              <li
+                key={i}
+                className="hover:text-white p-5 rounded hover:bg-[#6556CD] duration-300 cursor-pointer"
+              >
+                <Link to={`/${category}/details/${c.id}`} className="">
+                  <span>{c.title || c.original_name || c.original_title}</span>
+
+                  <span className="block ml-5">
+                    {c.character && `Charater Name: ${c.character}`}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   ) : (
     <Loading />
   );
--- src/components/partials/Cards.jsx
+++ src/components/partials/Cards.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import noimage from "/no_image.png"
+import noimage from "/no_image.png";
 
 const Cards = ({ data, title }) => {
   return (
     <div className="flex flex-wrap w-full h-full px-[5%] bg-[#1F1E24]">
       {data.map((c, i) => (
-        <Link to={`/${c.media_type || title}/details/${c.id}`} key={i} className="relative w-[25vh] mr-[5%] mb-[5%]">
-          <img
-            className="shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover"
-            src={c.poster_path || c.backdrop_path || c.profile_path ? `https://image.tmdb.org/t/p/original/${
-              c.poster_path || c.backdrop_path || c.profile_path
-            }` : noimage}
-            alt=""
-          />
-          <h1 className="text-2xl text-zinc-300 mt-3 font-semibold">{c.name || c.title || c.orginal_name || c.orginal_title}</h1>
-          {/* {(c.vote_average &&  <div className="absolute right-[-12%] bottom-[35%] rounded-full text-sm font-semibold bg-yellow-600 text-white w-[5vh] h-[5vh] flex justify-center items-center">{(c.vote_average * 10).toFixed()} <sup>%</sup> </div> )} */}
+        <Link
+          to={`/${c.media_type || title}/details/${c.id}`}
+          key={i}
+          className="relative w-[25vh] mr-[5%] mb-[5%] transform transition-transform duration-300 hover:scale-105"
+        >
+          <div className="relative">
+            <img
+              className="shadow-lg rounded-lg h-[40vh] object-cover"
+              src={
+                c.poster_path || c.backdrop_path || c.profile_path
+                  ? `https://image.tmdb.org/t/p/original/${
+                      c.poster_path || c.backdrop_path || c.profile_path
+                    }`
+                  : noimage
+              }
+              alt={c.name || c.title || c.original_name || c.original_title}
+            />
+            {c.vote_average && (
+              <div className="absolute top-2 right-2 bg-yellow-600 text-white text-sm font-semibold rounded-full w-10 h-10 flex items-center justify-center">
+                {(c.vote_average * 10).toFixed()}%
+              </div>
+            )}
+          </div>
+          <h1 className="text-xl text-zinc-300 mt-3 font-semibold truncate">
+            {c.name || c.title || c.original_name || c.original_title}
+          </h1>
         </Link>
       ))}
     </div>
